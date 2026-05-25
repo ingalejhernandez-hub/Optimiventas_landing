870ef1558508e16717278c929b9718c375094ce0
ingalejhernandez-hub <ingalejhernandez@gmail.com>
2026-05-25 08:05:23 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+
+export default function App() {
+  return (
+    <main style={{fontFamily:"Arial",padding:"40px"}}>
+      <h1>Optimiventas SRL</h1>
+      <p>Landing React compilable incluida correctamente.</p>
+    </main>
+  );
+}
